05b47255fcebdb3b5a8bb2981469efddf984965f
Shalani Kuruppu <shalanikuruppu08@gmail.com>
2024-08-20 16:27:54 +0530

    admin-auth and user-auth created

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserAuthModule } from './user-auth/user-auth.module';
+import { AdminAuthModule } from './admin-auth/admin-auth.module';
 import config from './config/keys';
 
 @Module({
   imports: [MongooseModule.forRoot(config.mongoURI,{
     autoCreate: true
-    })],
+    }), UserAuthModule, AdminAuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
