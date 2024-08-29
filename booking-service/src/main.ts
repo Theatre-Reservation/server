7664cd9837bbb5bd58397e8df178d9d99603cb66
Leshmith <leshmith1212@gmail.com>
2024-08-29 19:04:58 +0530

    Merged manually

--- booking-service/src/main.ts
+++ booking-service/src/main.ts
@@ -3,10 +3,10 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
+  const cors = require('cors');
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
-  app.enableCors();
-  await app.listen(8001);
+  app.use(cors());
+  await app.listen(3000);
   
 }
 bootstrap();
