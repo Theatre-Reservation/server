dba7295850334b666a3ec9b4b47c51edc3a09445
SanjanaKumarasingha <Sanjanakumarasingha1@gmail.com>
2024-10-19 12:13:10 +0530

    Refactor movie module to remove DeleteMovie model and schema

--- admin-service/src/movie/movie.module.ts
+++ admin-service/src/movie/movie.module.ts
@@ -3,13 +3,10 @@ import { MovieController } from './movie.controller';
 import { MovieService } from './movie.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Movie, MovieSchema } from 'src/db/movie.model';
-import { DeleteMovie, DeleteMovieSchema } from 'src/db/deleteMovieModel';
-
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Movie.name, schema: MovieSchema },
-      {name: DeleteMovie.name, schema: DeleteMovieSchema} 
     ])
   ],
   controllers: [MovieController],
