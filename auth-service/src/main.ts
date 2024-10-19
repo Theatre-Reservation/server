8ea19738b4508e4e473492bcf6c6899e91157847
Leshmith <leshmith1212@gmail.com>
2024-10-19 11:53:04 +0530

    Allowed all URLs to access services. Added docker files to each service.

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -2,19 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
-
 async function bootstrap() {
   const cors = require('cors');
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
   app.setGlobalPrefix('api/v1');
+
   app.use(cors({
-    origin: ['http://localhost:5173','https://flash-ticket-admin.netlify.app'],// Allow this specific origin
-    credentials: true,               // Allow credentials
+    origin: (origin, callback) => {
+      callback(null, origin);  // Allow all origins
+    },
+    credentials: true,  // Allow credentials
   }));
-  
-  await app.listen(8500); // Start the HTTP server on port 8500
+
+  await app.listen(8500);  // Start the HTTP server on port 8500
 }
 
 bootstrap();
